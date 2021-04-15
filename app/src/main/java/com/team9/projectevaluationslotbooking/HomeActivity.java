0aa7eb1c8d03ec42e156383f1959b8acd845086a
Rehan Raza <rhn.raza121@gmail.com>
2021-04-15 14:06:46 +0530

    homeActivity student name

--- app/src/main/java/com/team9/projectevaluationslotbooking/HomeActivity.java
+++ app/src/main/java/com/team9/projectevaluationslotbooking/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.team9.projectevaluationslotbooking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,7 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -18,10 +23,10 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-//        if(FirebaseAuth.getInstance()==null) {
-//            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-//            startActivity(intent);
-//        }
+        if(FirebaseAuth.getInstance().getCurrentUser()==null) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -35,7 +40,17 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
-        tvWelcome.setText("Welcome "+ FirebaseAuth.getInstance().getCurrentUser().getUid());
+        FirebaseDatabase.getInstance().getReference("Student")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+
+                        if(task.isSuccessful()) {
+                            tvWelcome.setText("Welcome " + String.valueOf(task.getResult().child("fname").getValue()));
+                        }
+                    }
+                });
+
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
